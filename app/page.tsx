09541c40b4e2bbe5060b244d3efb20cbfa9ce398
SamW314 <yuhanwang-inaus@hotmail.com>
2025-09-04 19:15:36 +1000

    implemented about section

--- app/page.tsx
+++ app/page.tsx
@@ -70,25 +70,25 @@ export default function Home() {
         </div>
       </div>
       */}
-      {/* about */}
-      <div className="p-6 max-w-4xl mx-auto mt-10 bg-blue-900">
-        <h2 className="text-xl font-semibold mb-4 text-white">Who are we?</h2>
-        <p className="mb-6 text-white">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et
-          lorem at enim ultrices congue. Nullam tincidunt volutpat nunc nec
-          suscipit. Aenean fringilla leo sed sapien tincidunt, vel malesuada nulla
-          faucibus. Maecenas et augue sed lorem fermentum sodales.
-        </p>
-        <div className="flex flex-wrap gap-4">
-          <button className="border border-gray-500 px-4 py-2 rounded hover:bg-blue-950">
-            Sign up
-          </button>
-          <button className="border border-gray-500 px-4 py-2 rounded hover:bg-blue-950">
-            Join the Discord
-          </button>
-          <button className="border border-gray-500 px-4 py-2 rounded hover:bg-blue-950">
-            Follow our socials
-          </button>
+      {/* about */}      
+      <div className="flex flex-row gap-[41px] items-start px-[31px] py-[72px] h-[589px] bg-[#1A2223] ">
+        <div className="relative w-[390px] h-[446px]">
+          <Image src={slides[0]} alt="placeholder" fill className="object-cover"/>
+        </div>
+        
+        <div className="w-[740px] h-[450px] flex flex-col justify-center space-y-4">
+          <h2 className="bold text-[36px] mb-4 text-white">About Game Maker Club</h2>
+          <p className="text-white bold text-[24px] max-w-[732px] self-end">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et
+            lorem at enim ultrices congue. Nullam tincidunt volutpat nunc nec
+            suscipit. Aenean fringilla leo sed sapien tincidunt, vel malesuada nulla
+            faucibus. Maecenas et augue sed lorem fermentum sodales.Aenean fringilla 
+            leo sed sapien tincidunt, vel malesuada nulla faucibus. Maecenas et augue 
+            sed lorem fermentum sodales.
+          </p>
+            <button className="w-[187px] h-[66px] bg-[#4FA0CF] px-4 py-2 rounded hover:bg-[#266b94] text-[#F7F6F3] self-center">
+              About Us →
+            </button>
         </div>
       </div>
       {/* education */}
