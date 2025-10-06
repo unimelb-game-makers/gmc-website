67f40a708303e09fc5bdef5becdbcb10e8b7fd79
junyynyyn <7540864+junyynyyn@users.noreply.github.com>
2025-10-06 14:55:12 +1100

    Added social links to website footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -4,14 +4,20 @@ import Image from 'next/image'
 
 const Footer = () => {
   return (
-    <div className='flex space-x-[15px] p-[30px] bg-[#012E65] w-full h-[130px] font-karla'>
+    <div className='flex justify-center space-x-[15px] p-[30px] bg-[#012E65] w-full h-[100px] font-karla'>
         {/* logo area */}
-        <div className='flex'>
-            <Image src='/gmc_logo.png' alt='gmc_logo' width={72} height={72} />
-        </div>
-        <div className='flex'>
-            <Image src='/gmc_logo.png' alt='gmc_logo' width={72} height={72} />
-        </div>
+        <a href="https://www.instagram.com/gmc.unimelb/">
+          <Image src="/icons/instagram.png" alt="instagram" width={40} height={40} />
+        </a>
+        <a href="https://gmcunimelb.itch.io/">
+          <Image src="/icons/itch.svg" alt="itch" width={40} height={40} />
+        </a>
+        <a href="http://discord.gg/NQEnBhsEGG">
+          <Image src="/icons/discord.svg" alt="discord" width={40} height={40} />
+        </a>
+        <a href="https://www.linkedin.com/company/unimelb-game-makers-club/">
+          <Image src="/icons/linkedin.png" alt="linkedin" width={40} height={40} />
+        </a>
     </div>
   )
 }
