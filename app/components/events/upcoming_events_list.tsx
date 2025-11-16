09b95168a6de5fa17993c2159eaa984ba5519c5f
junyynyyn <7540864+junyynyyn@users.noreply.github.com>
2025-11-16 20:12:40 +1100

    Feature: Added events page, upcoming/past events selector.

---
+++ app/components/events/upcoming_events_list.tsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+const upcoming_events = async () => {
+  return (
+    <div>upcoming_events</div>
+  )
+}
+
+export default upcoming_events
--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,27 +1,62 @@
+"use client";
 import React from 'react'
+import { useEffect, useState } from 'react'
 
-const page = async () => {
+const page = () => {
+  const [active, setActive] = useState(true);
 
-  // const databaseId = process.env.NOTION_EVENTS
-  // const response = await notion.databases.query({
-  //           database_id: databaseId,
-  //           filter: {
-  //               property: 'Public Checkbox',
-  //               checkbox: {
-  //                   equals: true,
-  //               }
-  //           },
-  //           sorts: [
-  //               {
-  //                   property: 'Event Date',
-  //                   direction: 'descending',
-  //               }
-  //           ]
-  //       });
-  
-  // console.log(response);
   return (
-    <div>Events Page</div>
+    <div className='m-10'>
+      <h1 className='text-7xl font-bold'>Events</h1>
+      {/* <div className="hidden lg:flex bg-[#D9D9D9] rounded-lg p-1 justify-center space-x-2">
+          {committees.map((committee) => (
+            <button
+              key={committee}
+              onClick={() => setSelectedCommittee(committee)}
+              className={`px-4 py-2 rounded-md transition-transform duration-100 ease-out transform hover:scale-105 hover:-translate-y-0.5 cursor-pointer text-lg font-bold ${
+                selectedCommittee === committee ? "text-white" : "text-black"
+              }`}
+              style={{
+                backgroundColor:
+                  selectedCommittee === committee ? "#012E65" : "transparent",
+              }}
+            >
+              {committee.toUpperCase()}
+            </button>
+          ))}
+        </div> */}
+      <div className="bg-[#D9D9D9] rounded-lg p-1 inline-flex">
+        <button
+          onClick={() => setActive(true)}
+          className={`px-4 py-2 rounded-md transition-transform duration-100 ease-out transform hover:scale-105 hover:-translate-y-0.5 cursor-pointer text-lg font-bold
+            ${active === true 
+              ? "bg-blue-900 text-white" 
+              : "text-gray-700"
+            }`}
+        >
+          Upcoming
+        </button>
+
+        <button
+          onClick={() => setActive(false)}
+          className={`px-4 py-2 rounded-md transition-transform duration-100 ease-out transform hover:scale-105 hover:-translate-y-0.5 cursor-pointer text-lg font-bold
+            ${active === false
+              ? "bg-blue-900 text-white" 
+              : "text-gray-700"
+            }`}
+        >
+          Previous
+        </button>
+      </div>
+
+      <div>
+        Events
+      </div>
+
+      <div>
+        <h1 className='text-5xl font-bold'>Regular Events</h1>
+      </div>
+    </div>
   )
 }
 
