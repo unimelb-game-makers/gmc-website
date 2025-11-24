1411aa4831609dc0386726cb8b0c9e61f252a0ee
junyynyyn <7540864+junyynyyn@users.noreply.github.com>
2025-11-24 13:48:16 +1100

    Fix: Adjusted switch to center of page.

--- app/components/events/events_switch.tsx
+++ app/components/events/events_switch.tsx
@@ -16,28 +16,30 @@ const EventsSwitch = ({events}: {events: Event[]}) => {
     const previousEvents = events.filter(event => dayjs(event.date.start).isBefore(date));
     return (
         <div className='m-2'>
-            <div className="bg-[#D9D9D9] rounded-lg p-1 inline-flex">
-                <button
-                onClick={() => setEventList(true)}
-                className={`px-4 py-2 rounded-md transition-transform duration-100 ease-out transform hover:scale-105 hover:-translate-y-0.5 cursor-pointer text-lg font-bold
-                    ${eventList === true 
-                    ? "bg-blue-900 text-white" 
-                    : "text-gray-700"
-                    }`}
-                >
-                Upcoming
-                </button>
+            <div className="flex justify-center">
+                <div className="bg-[#D9D9D9] rounded-lg p-1 inline-flex">
+                    <button
+                    onClick={() => setEventList(true)}
+                    className={`px-4 py-2 rounded-md transition-transform duration-100 ease-out transform hover:scale-105 hover:-translate-y-0.5 cursor-pointer text-lg font-bold
+                        ${eventList === true
+                        ? "bg-blue-900 text-white"
+                        : "text-gray-700"
+                        }`}
+                    >
+                    Upcoming
+                    </button>
 
-                <button
-                onClick={() => setEventList(false)}
-                className={`px-4 py-2 rounded-md transition-transform duration-100 ease-out transform hover:scale-105 hover:-translate-y-0.5 cursor-pointer text-lg font-bold
-                    ${eventList === false
-                    ? "bg-blue-900 text-white" 
-                    : "text-gray-700"
-                    }`}
-                >
-                Previous
-                </button>
+                    <button
+                    onClick={() => setEventList(false)}
+                    className={`px-4 py-2 rounded-md transition-transform duration-100 ease-out transform hover:scale-105 hover:-translate-y-0.5 cursor-pointer text-lg font-bold
+                        ${eventList === false
+                        ? "bg-blue-900 text-white"
+                        : "text-gray-700"
+                        }`}
+                    >
+                    Previous
+                    </button>
+                </div>
             </div>
 
             <div>
--- app/events/page.tsx
+++ app/events/page.tsx
@@ -15,16 +15,16 @@ const page = async () => {
         <div>
           <h1 className='text-5xl font-bold mt-5'>Regular Events</h1>
 
-          <h1 className='text-5xl font-bold m-10'>Weekly Maker Lab</h1>
-          <div className='bg-gray-300 rounded-2xl p-10 mx-10 max-w-[1400px] flex justify-between text-black'>
-            <p className='text-3xl'>Regular meetings every week, where we get together to discuss all aspects of creating games.</p>
-            <Image src={"/images/cat.jpg"} alt={"makerlab"} width={400} height={300} className='rounded-2xl'/>
+          <h1 className='text-4xl font-bold my-10'>Weekly Maker Lab</h1>
+          <div className='bg-gray-300 rounded-2xl p-10 mx-10 max-w-[1400px] flex flex-col md:flex-row justify-between text-black'>
+            <p className='text-2xl'>Regular meetings every week, where we get together to discuss all aspects of creating games.</p>
+            <Image src={"/images/cat.jpg"} alt={"makerlab"} width={400} height={300} className='rounded-2xl md:ml-6 mt-6 md:mt-0'/>
           </div>
 
-          <h1 className='text-5xl font-bold m-10 mt-20 text-right'>Regular Game Jams</h1>
-          <div className='bg-gray-300 rounded-2xl p-10 mx-10 max-w-[1400px] flex justify-between text-black ml-auto'>
-            <Image src={"/images/cat.jpg"} alt={"makerlab"} width={400} height={300} className='rounded-2xl'/>
-            <p className='text-3xl ml-10'>Bi-Semesterly game jams where people can compete to earn prizes, or just for the fun of the game.</p>
+          <h1 className='text-4xl font-bold m-10 mt-20 text-right'>Regular Game Jams</h1>
+          <div className='bg-gray-300 rounded-2xl p-10 mx-10 max-w-[1400px] flex flex-col md:flex-row justify-between text-black ml-auto'>
+            <Image src={"/images/cat.jpg"} alt={"makerlab"} width={400} height={300} className='rounded-2xl md:mr-6 mt-6 md:mt-0 md:order-first order-last'/>
+            <p className='text-2xl md:ml-10'>Bi-Semesterly game jams where people can compete to earn prizes, or just for the fun of the game.</p>
           </div>
           
         </div>
