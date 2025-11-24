b7f5da815ca1562f15ea969e028a6668e6262ac1
junyynyyn <7540864+junyynyyn@users.noreply.github.com>
2025-11-24 13:30:31 +1100

    Fix: Event Info div sizing

--- app/components/events/event_info.tsx
+++ app/components/events/event_info.tsx
@@ -9,7 +9,7 @@ const EventInfo = ({event, onClose}: {event: Event, onClose: ()=>void}) => {
     <div className='fixed inset-0 z-50 flex items-center justify-center text-black  '>
       <div className="absolute inset-0 bg-black/50" onClick={onClose}/>
       <div
-        className="relative bg-white rounded-lg shadow-xl p-5 max-w-xl w-full mx-4 z-10"
+        className="relative bg-white rounded-lg shadow-xl p-5 max-w-xl w-full mx-4 z-10 h-[450px] flex flex-col"
       >
         <button
           onClick={onClose}
@@ -19,8 +19,8 @@ const EventInfo = ({event, onClose}: {event: Event, onClose: ()=>void}) => {
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
           </svg>
         </button>
-        <h1 className='text-2xl font-bold'>{event.name}</h1>
-        <div className="w-full h-48 rounded-md overflow-hidden mt-1">
+        <h1 className='text-2xl font-bold flex-shrink-0'>{event.name}</h1>
+        <div className="w-full h-48 rounded-md overflow-hidden mt-1 flex-shrink-0">
             <Image
             src={event.thumbnail? event.thumbnail : "/images/wires.png"} // replace with your image
             alt="Event Banner"
@@ -29,16 +29,18 @@ const EventInfo = ({event, onClose}: {event: Event, onClose: ()=>void}) => {
             className="object-cover w-full h-full"
             />
         </div>
-        <p>{event.description}</p>
+        <div className="overflow-y-auto flex-1 my-3">
+          <p>{event.description}</p>
+        </div>
         {/* Location */}
-        <div className="flex items-start h-8 mt-5">
+        <div className="flex items-start h-8 mt-2 flex-shrink-0">
             <Image className="mr-[5px]" src="/Mappin.svg" alt="map pin logo" width={24} height={24}/>
             {/* some location in the database is too long */}
             {/* <p>{location ? location.split('\n')[0] : 'TBA'}</p> */}
             <p>{event.location ? (event.location.length < 35 ? event.location : event.location.slice(0, 35) + "...") : 'TBA'}</p>
         </div>
         {/* Time */}
-        <div className="flex">
+        <div className="flex flex-shrink-0">
             <Image className="mr-[5px]" src="/Clock.svg" alt="clock logo" width={24} height={24}/>
             <p>{eventTime != "00:00 AM" ? eventTime : "TBD"}</p>
         </div>
