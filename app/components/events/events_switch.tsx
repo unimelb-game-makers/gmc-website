dd6aa787e5ea8e33b70ca2d23c29b11551a463b2
junyynyyn <7540864+junyynyyn@users.noreply.github.com>
2026-01-31 17:19:08 +0800

    Update: Some cool visuals/animations
    might be too much tbh

--- app/components/events/events_switch.tsx
+++ app/components/events/events_switch.tsx
@@ -7,6 +7,8 @@ import EventInfo from './event_info';
 import { Event } from '@/@types/schema.ds';
 import dayjs from 'dayjs';
 
+import { FaHourglass, FaHourglassHalf } from 'react-icons/fa';
+
 const EventsSwitch = ({events}: {events: Event[]}) => {
     const [eventList, setEventList] = useState(true);
     const [selectedEvent, setSelectedEvent] = useState<Event | null>(null);
@@ -17,19 +19,38 @@ const EventsSwitch = ({events}: {events: Event[]}) => {
     return (
         <div className='m-2'>
             <div className='flex items-start'>
-                <h1 className="text-vertical-textured text-6xl">
-                    Upcoming
-                </h1>
+                <div className="relative overflow-hidden">
+                    <h1 className={`text-vertical-textured text-6xl transition-transform duration-300 ease-in-out ${eventList ? 'translate-y-0' : '-translate-y-full'}`}>
+                        Upcoming
+                    </h1>
+                    <h1 className={`text-vertical-textured text-6xl transition-transform duration-300 ease-in-out absolute top-0 left-0 ${!eventList ? 'translate-y-0' : 'translate-y-full'}`}>
+                        Previous
+                    </h1>
+                </div>
                 <div className='ml-4'>
-                    <button className='flex' onClick={()=> {setEventList(!eventList)}}>
-                    <div className={`w-10 h-10 ${eventList ? 'bg-gmc-orange-dark' : 'bg-white'}`} />
-                    <div className={`w-10 h-10 ${eventList ? 'bg-white' : 'bg-gmc-orange-dark'}`} />
+                    <button className='relative flex w-20 h-10' onClick={()=> {setEventList(!eventList)}}>
+                        {/* Background track */}
+                        <div className='absolute inset-0 flex'>
+                            <div className='w-10 h-10 bg-white' />
+                            <div className='w-10 h-10 bg-white' />
+                        </div>
+                        {/* Sliding indicator */}
+                        <div className={`absolute w-10 h-10 bg-gmc-orange-dark flex items-center justify-center transition-transform duration-300 ease-in-out ${eventList ? 'translate-x-0' : 'translate-x-10'}`}>
+                            <FaHourglass className='text-xl text-white' />
+                        </div>
                     </button>
                     <div className="w-20 h-100 bg-gmc-orange mt-8" />
                 </div>
                 
-                <div className='h-200 overflow-y-auto ml-10 mr-10 flex-1'>
-                    {eventList ? <UpcomingEventsList events={upcomingEvents} setEvent={setSelectedEvent}/> : <PreviousEventList events={previousEvents} setEvent={setSelectedEvent}/>}
+                <div className='h-200 overflow-y-auto ml-10 mr-10 flex-1 relative'>
+                    {/* Upcoming Events */}
+                    <div className={`transition-opacity duration-300 ease-in-out ${eventList ? 'opacity-100' : 'opacity-0 absolute inset-0 pointer-events-none'}`}>
+                        <UpcomingEventsList events={upcomingEvents} setEvent={setSelectedEvent}/>
+                    </div>
+                    {/* Previous Events */}
+                    <div className={`transition-opacity duration-300 ease-in-out ${!eventList ? 'opacity-100' : 'opacity-0 absolute inset-0 pointer-events-none'}`}>
+                        <PreviousEventList events={previousEvents} setEvent={setSelectedEvent}/>
+                    </div>
                 </div>
 
             </div>
