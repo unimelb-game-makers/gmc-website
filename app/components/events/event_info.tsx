ae1cd7b8e25b53627f7a7d97e56ce112505def27
junyynyyn <7540864+junyynyyn@users.noreply.github.com>
2026-02-26 22:46:40 +0800

    change: Event info is now 4:5 ratio on focused view

--- app/components/events/event_info.tsx
+++ app/components/events/event_info.tsx
@@ -11,7 +11,7 @@ const EventInfo = ({ event, onClose }: { event: Event, onClose: () => void }) =>
     <div className='fixed inset-0 z-50 flex items-center justify-center text-black  '>
       <div className="absolute inset-0 bg-black/50" onClick={onClose} />
       <div
-        className="relative bg-gmc-cream rounded-lg shadow-xl p-5 max-w-xl w-full mx-4 z-10 h-[450px] flex flex-col"
+        className="relative bg-gmc-cream rounded-lg shadow-xl p-5 max-w-xl w-full mx-4 z-10 flex flex-col"
       >
         <button
           onClick={onClose}
@@ -22,30 +22,34 @@ const EventInfo = ({ event, onClose }: { event: Event, onClose: () => void }) =>
           </svg>
         </button>
         <h1 className='text-2xl font-bold shrink-0'>{event.name}</h1>
-        <div className="relative w-full h-48 bg-gray-400 rounded-md overflow-hidden mt-1 shrink-0 flex items-center justify-center">
-          {event.thumbnail ? (
-            <LoadingImage
-              src={event.thumbnail}
-              alt="Event Banner"
-              width={300}
-              height={200}
-              spinnerSize="w-8 h-8"
-              className="object-cover w-full h-full"
-            />
-          ) : null}
-        </div>
-        <div className="overflow-y-auto flex-1 my-3">
-          <p>{event.description}</p>
-        </div>
-        {/* Location */}
-        <div className="flex items-start h-8 mt-2 shrink-0">
-          <Image className="mr-[5px]" src="/Mappin.svg" alt="map pin logo" width={24} height={24} />
-          <p>{event.location ? event.location : 'TBA'}</p>
-        </div>
-        {/* Time */}
-        <div className="flex shrink-0">
-          <Image className="mr-[5px]" src="/Clock.svg" alt="clock logo" width={24} height={24} />
-          <p>{eventTime != "00:00 AM" ? eventTime : "TBD"}</p>
+        <div className="flex flex-col sm:flex-row gap-4 mt-2 flex-1 min-h-0">
+          {/* Image */}
+          <div className="relative w-3/5 mx-auto sm:mx-0 sm:w-1/2 shrink-0 aspect-4/5 bg-gray-400 rounded-md overflow-hidden self-start">
+            {event.thumbnail ? (
+              <LoadingImage
+                src={event.thumbnail}
+                alt="Event Banner"
+                width={400}
+                height={500}
+                spinnerSize="w-8 h-8"
+                className="object-cover w-full h-full"
+              />
+            ) : null}
+          </div>
+          {/* Right: description + meta */}
+          <div className="flex flex-col flex-1 min-h-0">
+            <div className="overflow-y-auto">
+              <p>{event.description}</p>
+            </div>
+            <div className="flex items-start mt-2 shrink-0">
+              <Image className="mr-1.25 shrink-0" src="/Mappin.svg" alt="map pin logo" width={24} height={24} />
+              <p>{event.location ? event.location : 'TBA'}</p>
+            </div>
+            <div className="flex mt-1 shrink-0">
+              <Image className="mr-1.25 shrink-0" src="/Clock.svg" alt="clock logo" width={24} height={24} />
+              <p>{eventTime != "00:00 AM" ? eventTime : "TBD"}</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
