001f6fa0b791595507687d999b056d22a47006ad
junyynyyn <7540864+junyynyyn@users.noreply.github.com>
2026-06-30 18:45:04 +0800

    Gallery Boilerplate

---
+++ app/gallery/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+        <div className='py-40'>
+            Gallery Home Page
+        </div>
+    </>
+  )
+}
+
+export default page
