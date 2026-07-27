0ccfc11819ebbb2baebfb001cef0446a9ffc1305
Jun <junyyeo@gmail.com>
2026-07-27 16:56:33 +0800

    refactor: flatten migrations, added user_id instead of attempting direct link to supabase auth

--- prisma/migrations/20260629160635_fix/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE "Game" ALTER COLUMN "thumbnail" DROP NOT NULL,
-ALTER COLUMN "link" DROP NOT NULL,
-ALTER COLUMN "description" DROP NOT NULL;
-
--- AlterTable
-ALTER TABLE "Tag" ALTER COLUMN "description" DROP NOT NULL;
--- services/games-gallery/creator.service.ts
+++ services/games-gallery/creator.service.ts
@@ -1,6 +1,12 @@
 import { db } from "@/lib/db";
 
-export async function getCreatorById(id: number) {
+export async function getCreators() {
+    const creators = await db.creator.findMany();
+
+    return creators;
+}
+
+export async function getCreatorById(id: string) {
 const creator = await db.creator.findUnique({
     where: { id },
     });
