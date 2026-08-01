009de96c60ae904282582d2487f1bab9759ccb24
Jun <junyyeo@gmail.com>
2026-08-01 16:48:52 +0800

    feat: get creator by user id

--- services/games-gallery/creator.service.ts
+++ services/games-gallery/creator.service.ts
@@ -7,8 +7,15 @@ export async function getCreators() {
 }
 
 export async function getCreatorById(id: string) {
-const creator = await db.creator.findUnique({
-    where: { id },
+    const creator = await db.creator.findUnique({
+        where: { id },
+    });
+    return creator;
+}
+
+export async function getCreatorByUserId(id: string) {
+    const creator = await db.creator.findUnique({
+        where: { user_id: id },
     });
     return creator;
 }
