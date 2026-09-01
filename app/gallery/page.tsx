88ef4589daebcf289787233bbec062a64fef4ddd
Jun <junyyeo@gmail.com>
2026-09-01 15:08:22 +0800

    change: randomized featured on main gallery page

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,250 +1,27 @@
 import React from 'react'
 
-import FeaturedGameCarousel, { type GalleryGame, type GalleryTag } from '../components/gallery/featured-game-carousel'
+import FeaturedGameCarousel, { type GalleryGame } from '../components/gallery/featured-game-carousel'
 import GalleryBrowser from '../components/gallery/gallery-browser'
 import { getGames } from '@/services/games-gallery/game.service'
 import { getFeaturedTags } from '@/services/games-gallery/tag.service'
 
-const tagRegistry = new Map<string, GalleryTag>()
-let nextTagId = 1
+const FEATURED_GAME_COUNT = 4
 
-const toTags = (tagNames: string[]): GalleryTag[] =>
-  tagNames.map((name) => {
-    const existingTag = tagRegistry.get(name)
+const pickRandomGames = (games: GalleryGame[], count: number) => {
+  const shuffled = [...games]
 
-    if (existingTag) {
-      return existingTag
-    }
+  for (let index = shuffled.length - 1; index > 0; index -= 1) {
+    const swapIndex = Math.floor(Math.random() * (index + 1))
+    ;[shuffled[index], shuffled[swapIndex]] = [shuffled[swapIndex], shuffled[index]]
+  }
 
-    const tag: GalleryTag = { id: nextTagId, name, description: name }
-
-    nextTagId += 1
-    tagRegistry.set(name, tag)
-
-    return tag
-  })
-
-const galleryGames: GalleryGame[] = [
-  {
-    name: 'Anatis',
-    description: '2025 Mid Sem Winner',
-    thumbnail: '/images/games_examples/game27.png',
-    tags: toTags(['Puzzle', 'Casual', 'Fantasy']),
-    creators: [
-      {
-        name: 'GMC Artist',
-        picture: '/images/gmc_site_avatar.png',
-        about: 'A featured student from Game Maker\'s Club.',
-      },
-      {
-        name: 'GMC Programmer',
-        picture: '/images/gmc-cat.png',
-        about: 'A featured student from Game Maker\'s Club.',
-      },
-    ],
-  },
-  {
-    name: 'Neon Relay',
-    description: '2025 Mid Sem Winner',
-    thumbnail: '/images/games_examples/game2.png',
-    tags: toTags(['Arcade', 'Speed', 'Sci-Fi']),
-    creators: [
-      {
-        name: 'GMC Designer',
-        picture: '/images/gmc-cat-whiteboard.png',
-        about: 'A featured student from Game Maker\'s Club.',
-      },
-    ],
-  },
-  {
-    name: 'Petal Shift',
-    description: '2025 Mid Sem Winner',
-    thumbnail: '/images/games_examples/game3.png',
-    tags: toTags(['Strategy', 'Fantasy', 'Card']),
-    creators: [
-      {
-        name: 'GMC Illustrator',
-        picture: '/images/committee.png',
-        about: 'A featured student from Game Maker\'s Club.',
-      },
-    ],
-  },
-  {
-    name: 'Velvet Zero',
-    description: '2025 Mid Sem Winner',
-    thumbnail: '/images/games_examples/game4.png',
-    tags: toTags(['Action', 'Sci-Fi', 'Boss Rush']),
-    creators: [
-      {
-        name: 'GMC Developer',
-        picture: '/images/gmc-cat-mail.png',
-        about: 'A featured student from Game Maker\'s Club.',
-      },
-    ],
-  },
-  {
-    name: 'Cassidy Dungeon',
-    description: 'Winter Jam 2026',
-    thumbnail: '/images/games_examples/game1.png',
-    tags: toTags(['Dungeon', 'Pixel', 'Action']),
-    creators: [
-      {
-        name: 'GMC Designer',
-        picture: '/images/gmc-cat-whiteboard.png',
-        about: 'A featured student from Game Maker\'s Club.',
-      },
-    ],
-  },
-  {
-    name: 'Frostbyte Sprint',
-    description: 'Winter Jam 2026',
-    thumbnail: '/images/games_examples/game5.png',
-    tags: toTags(['Platformer', 'Winter', 'Arcade']),
-    creators: [
-      {
-        name: 'GMC Artist',
-        picture: '/images/gmc_site_avatar.png',
-        about: 'A featured student from Game Maker\'s Club.',
-      },
-    ],
-  },
-  {
-    name: 'Lantern Loop',
-    description: 'Winter Jam 2026',
-    thumbnail: '/images/games_examples/game6.png',
-    tags: toTags(['Puzzle', 'Cozy', 'Adventure']),
-    creators: [
-      {
-        name: 'GMC Programmer',
-        picture: '/images/gmc-cat.png',
-        about: 'A featured student from Game Maker\'s Club.',
-      },
-    ],
-  },
-  {
-    name: 'Snowglobe Signal',
-    description: 'Winter Jam 2026',
-    thumbnail: '/images/games_examples/game7.png',
-    tags: toTags(['Narrative', 'Winter', 'Mystery']),
-    creators: [
-      {
-        name: 'GMC Developer',
-        picture: '/images/gmc-cat-mail.png',
-        about: 'A featured student from Game Maker\'s Club.',
-      },
-    ],
-  },
-  {
-    name: 'Clockwork Orbit',
-    description: 'GMC x CISSA 2026',
-    thumbnail: '/images/games_examples/game25.png',
-    tags: toTags(['Arcade', 'Precision', 'Jam']),
-    creators: [
-      {
-        name: 'GMC Developer',
-        picture: '/images/gmc-cat-mail.png',
-        about: 'A featured student from Game Maker\'s Club.',
-      },
-    ],
-  },
-  {
-    name: 'Wireframe Run',
-    description: 'GMC x CISSA 2026',
-    thumbnail: '/images/games_examples/game16.png',
-    tags: toTags(['Racing', 'Sci-Fi', 'Multiplayer']),
-    creators: [
-      {
-        name: 'GMC Programmer',
-        picture: '/images/gmc-cat.png',
-        about: 'A featured student from Game Maker\'s Club.',
-      },
-    ],
-  },
-  {
-    name: 'Switchblade Garden',
-    description: 'GMC x CISSA 2026',
-    thumbnail: '/images/games_examples/game18.png',
-    tags: toTags(['Action', 'Fantasy', 'Co-op']),
-    creators: [
-      {
-        name: 'GMC Illustrator',
-        picture: '/images/committee.png',
-        about: 'A featured student from Game Maker\'s Club.',
-      },
-    ],
-  },
-  {
-    name: 'Echo Cabinet',
-    description: 'GMC x CISSA 2026',
-    thumbnail: '/images/games_examples/game19.png',
-    tags: toTags(['Horror', 'Mystery', 'Puzzle']),
-    creators: [
-      {
-        name: 'GMC Artist',
-        picture: '/images/gmc_site_avatar.png',
-        about: 'A featured student from Game Maker\'s Club.',
-      },
-    ],
-  },
-  {
-    name: 'Eye Bloom',
-    description: '2025 Showcase',
-    thumbnail: '/images/games_examples/game20.png',
-    tags: toTags(['Experimental', 'Art', 'Casual']),
-    creators: [
-      {
-        name: 'GMC Illustrator',
-        picture: '/images/committee.png',
-        about: 'A featured student from Game Maker\'s Club.',
-      },
-    ],
-  },
-  {
-    name: 'Cardboard Crown',
-    description: '2025 Showcase',
-    thumbnail: '/images/games_examples/game21.png',
-    tags: toTags(['Adventure', 'Comedy', 'Card']),
-    creators: [
-      {
-        name: 'GMC Designer',
-        picture: '/images/gmc-cat-whiteboard.png',
-        about: 'A featured student from Game Maker\'s Club.',
-      },
-    ],
-  },
-  {
-    name: 'Moon Static',
-    description: '2025 Showcase',
-    thumbnail: '/images/games_examples/game23.png',
-    tags: toTags(['Sci-Fi', 'Narrative', 'Exploration']),
-    creators: [
-      {
-        name: 'GMC Programmer',
-        picture: '/images/gmc-cat.png',
-        about: 'A featured student from Game Maker\'s Club.',
-      },
-    ],
-  },
-  {
-    name: 'Prism Patrol',
-    description: '2025 Showcase',
-    thumbnail: '/images/games_examples/game24.png',
-    tags: toTags(['Shooter', 'Arcade', 'Color']),
-    creators: [
-      {
-        name: 'GMC Artist',
-        picture: '/images/gmc_site_avatar.png',
-        about: 'A featured student from Game Maker\'s Club.',
-      },
-    ],
-  },
-]
-
-const featuredGames = galleryGames.slice(0, 4)
+  return shuffled.slice(0, count)
+}
 
 const GalleryPage = async () => {
   const games = await getGames()
   const featuredTags = await getFeaturedTags()
+  const featuredGames = pickRandomGames(games, FEATURED_GAME_COUNT)
 
   return (
     <main className="min-h-screen bg-[#252525] px-4 pb-20 pt-44 sm:px-8 sm:pt-48 lg:px-12">
